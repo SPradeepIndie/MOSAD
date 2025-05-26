ad37f1e6d2927d430a44677822e365a02f88c5bf
Sanjaya Pradeep <152323246+SPradeepIndie@users.noreply.github.com>
2025-05-26 15:47:39 +0530

    Update apiClient.js

--- mosad_frontend/src/services/api_config/apiClient.js
+++ mosad_frontend/src/services/api_config/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //const BASE_URL= window?.configs?.apiUrl ? window.configs.apiUrl+"/api/v1" : import.meta.env.VITE_API_URL;
-const BASE_URL= ':8080/api/v1';
+const BASE_URL= '/api/v1';
 export const apiClient = axios.create({
     baseURL:BASE_URL,
     headers: {
