717e237a89a5dc1cd8c322899414068b46dfbe3f
Sanjaya Pradeep <152323246+SPradeepIndie@users.noreply.github.com>
2025-05-26 15:42:24 +0530

    Update apiClient.js

--- mosad_frontend/src/services/api_config/apiClient.js
+++ mosad_frontend/src/services/api_config/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //const BASE_URL= window?.configs?.apiUrl ? window.configs.apiUrl+"/api/v1" : import.meta.env.VITE_API_URL;
-const BASE_URL= '313dce28-5c14-4990-8e70-661764f55665-dev.e1-us-east-azure.choreoapis.dev/mosad-deploying/mosadbackend/api/v1';
+const BASE_URL= ':8080/api/v1';
 export const apiClient = axios.create({
     baseURL:BASE_URL,
     headers: {
